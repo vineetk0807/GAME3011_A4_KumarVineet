77bcaec3043a1aae8770289efedadfbce8a628db
Vineet Kumar <megazeroxzvk@gmail.com>
2022-04-04 23:51:45 -0400

    Added a cursor box to navigate using keys

---
+++ Assets/[Scripts]/CursorBehaviour.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Trigger detect - enter
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    /// <summary>
+    /// Trigger detect - exit
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        
+    }
+}
---
+++ Assets/[Scripts]/CursorBox.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorBox : MonoBehaviour
+{
+
+    [Header("Cursor Box")] 
+    public GameObject cursorBox;
+
+    [Header("Limit")]
+    public Vector2 verticalLimit;
+    public Vector2 horizontalLimit;
+
+    [Header("Factors to move")] 
+    public float verticalFactor;
+    public float horizontalFactor;
+    
+    [Header("Keycodes for Up and Down")]
+    public KeyCode UpKeyCode1;
+    public KeyCode UpKeyCode2;
+    public KeyCode DownKeyCode1;
+    public KeyCode DownKeyCode2;
+
+
+    [Header("Keycodes for Right and Left")]
+    public KeyCode RightKeyCode1;
+    public KeyCode RightKeyCode2;
+    public KeyCode LeftKeyCode1;
+    public KeyCode LeftKeyCode2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(UpKeyCode1) || Input.GetKeyDown(UpKeyCode2))
+        {
+            MoveLockPoint(1);
+        }
+        else if (Input.GetKeyDown(DownKeyCode1) || Input.GetKeyDown(DownKeyCode2))
+        {
+            MoveLockPoint(-1);
+        }
+
+        if (Input.GetKeyDown(RightKeyCode1) || Input.GetKeyDown(RightKeyCode2))
+        {
+            MoveCursor(1);
+        }
+        else if (Input.GetKeyDown(LeftKeyCode1) || Input.GetKeyDown(LeftKeyCode2))
+        {
+            MoveCursor(-1);
+        }
+    }
+
+    /// <summary>
+    /// MoveLockPoint function based on direction, -ve, +ve
+    /// </summary>
+    /// <param name="direction"></param>
+    private void MoveLockPoint(int direction)
+    {
+        if (direction > 0)
+        {
+            if (transform.position.y <= verticalLimit.y)
+            {
+                transform.position += new Vector3(0f, verticalFactor);
+            }
+        }
+        else
+        {
+            if (transform.position.y >= verticalLimit.x)
+            {
+                transform.position += new Vector3(0f, -verticalFactor);
+            }
+        }
+    }
+
+    private void MoveCursor(int direction)
+    {
+        if (direction > 0)
+        {
+            if (cursorBox.transform.position.x <= horizontalLimit.y)
+            {
+                cursorBox.transform.position += new Vector3(horizontalFactor, 0f);
+            }
+        }
+        else
+        {
+            if (cursorBox.transform.position.x >= horizontalLimit.x)
+            {
+                cursorBox.transform.position += new Vector3(-horizontalFactor, 0f);
+            }
+        }
+    }
+}
